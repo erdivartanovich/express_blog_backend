eaa2a8783ed9f6dba7e9ff70fdf018b743506aa6
erdivartanovich <erdivartanovich@gmail.com>
2017-09-29 13:55:54 +0700

    disable middleware

--- index.js
+++ index.js
@@ -9,19 +9,21 @@ const Posts = require('./src/domains/posts');
 
 //app config
 const port = 8081;
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-      "Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  res.header(
-      "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// config middleware
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//       "Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"
+//   );
+//   res.header(
+//       "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 //generate data
 const posts = new Posts();
